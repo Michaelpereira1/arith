3594504fbc00f4184f333a4ec5287b24f7d825fd
Hunter Larkin <hunter_larkin@homework.cs.uri.edu>
2021-11-07 14:48:04 -0500

    Fixed array2 issue, started to implement trim function, nothing is tested yet

--- rpeg/src/codec.rs
+++ rpeg/src/codec.rs
@@ -1,4 +1,4 @@
-use array2::Array2;
+use array2::array2::Array2;
 use csc411_image;
 
 pub fn compress(filename: &str) {
@@ -15,6 +15,14 @@ pub fn into_array(filename: &str){
     let ppm_array2 = Array2::from_row_major(img.width as usize, img.height as usize, img.pixels).unwrap();
 }
 
-pub fn trim(){
-    todo!();
+pub fn trim<T: Clone>(image: Array2<T>){
+    let image_width = Array2::width(&image);
+    let image_height = Array2::height(&image);
+    if image_width % 2 != 0 {
+        //trim the farthest right column from the image.
+        todo!();
+    }else if image_height % 2 != 0 {
+        //trim the bottom row from the image.
+        todo!();
+    }
 }
