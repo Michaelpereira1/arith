8c0a465c764876233e117f048602dc188652399e
Hunter Larkin <hunter_larkin@homework.cs.uri.edu>
2021-11-08 10:34:01 -0500

    working on trim function

--- rpeg/src/codec.rs
+++ rpeg/src/codec.rs
@@ -13,14 +13,19 @@ pub fn decompress(filename: &str) {
 pub fn into_array(filename: &str){
     let img = csc411_image::Image::read(Some(filename)).unwrap();
     let ppm_array2 = Array2::from_row_major(img.width as usize, img.height as usize, img.pixels).unwrap();
+    trim(ppm_array2);
 }
 
 pub fn trim<T: Clone>(image: Array2<T>){
-    let image_width = Array2::width(&image);
-    let image_height = Array2::height(&image);
+    let image_width: usize = Array2::width(&image);
+    let image_height: usize = Array2::height(&image);
+    
     if image_width % 2 != 0 {
         //trim the farthest right column from the image.
-        todo!();
+        //get_mut?
+        /*image.iter_row_major().
+            filter(|index| (index + 1) % image_width == 0).map(|index| todo!());*/
+
     }else if image_height % 2 != 0 {
         //trim the bottom row from the image.
         todo!();
